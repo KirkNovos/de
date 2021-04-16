4836fb2b646a43ef75c2d87d4802f6e2f098eb71
kei <kolei@ya.ru>
2021-04-16 14:29:58 +0300

    first

---
+++ Task3/Task3/MainWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task3
+{
+    class Core 
+    {
+        public static ekolesnikovEntities DB = new ekolesnikovEntities();
+    }
+
+    public partial class Product
+    {
+        public Uri ImagePreview
+        {
+            get
+            {
+                var imageName = System.IO.Path.Combine(Environment.CurrentDirectory, Image ?? "");
+                return System.IO.File.Exists(imageName) ? new Uri(imageName) : new Uri("pack://application:,,,/Images/picture.png");
+            }
+        }
+
+        public string MaterialsList
+        {
+            get
+            {
+                var Result = "";
+                foreach(var pm in ProductMaterial)
+                {
+                    Result += (Result=="" ? "" : ", ")+pm.Material.Title;
+                }
+                return Result;
+            }
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal Result = 0;
+                foreach (var pm in ProductMaterial)
+                    Result += pm.Material.Cost;
+                return Result;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private IEnumerable<Product> _ProductList;
+
+        private int _CurrentPage = 1;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int CurrentPage
+        {
+            get
+            {
+                return _CurrentPage;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    if (value <= _ProductList.Count() / 20)
+                    {
+                        _CurrentPage = value;
+                        Invalidate();
+                    }
+                }
+            }
+        }
+
+        private void Invalidate()
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ProductList"));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CurrentPage"));
+        }
+
+        public IEnumerable<Product> ProductList
+        {
+            get
+            {
+                return _ProductList.Skip((CurrentPage-1)*20).Take(20);
+            }
+            set
+            {
+                _ProductList = value;
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            ProductList = Core.DB.Product.ToArray();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void PrevPage_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentPage--;
+        }
+
+        private void NextPage_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentPage++;
+        }
+
+    }
+}
